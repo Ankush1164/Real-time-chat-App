ec1891a4d8326995b0d61ec71f38dfd922bf2e30
Ankush1164 <ankushdhiman028@gmail.com>
2024-01-06 10:24:58 +0530

    .

--- chatapp/src/Components/Chat.jsx
+++ chatapp/src/Components/Chat.jsx
@@ -6,7 +6,7 @@ import Message from './Message';
 import ReactScrollToBottom from "react-scroll-to-bottom"
 import CloseIcon from '@mui/icons-material/Close';
 
-const ENDPOINT = "https://real-time-chat-app-gules.vercel.app/"
+const ENDPOINT = "http://localhost:4000/"
 let socket;
 
 function Chat() {
